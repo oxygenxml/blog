039d90c90004a381d6bbe7cf03d6145d0f213bce
ionescumihai <mihai@sync.ro>
2022-07-13 12:59:06 +0300

    WEB-1698

--- out/webhelp-responsive/oxygen-webhelp/app/config.js
+++ out/webhelp-responsive/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220712175659';
+                args = '20220713125843';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
