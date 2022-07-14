e65f52c2dab3041550eb13d06cb1b09b1942f0f7
Radu Coravu <radu_coravu@sync.ro>
2022-07-14 10:12:55 +0300

    WEB-1698 Removed extra folders

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {6:14,4:12,13:12,20:14,72:-1,15:12,11:12,38:-1,8:14,40:39,17:11,21:12,10:12,19:14,113:-1,7:14,9:12,18:12,5:14,16:11,14:12};
-});
